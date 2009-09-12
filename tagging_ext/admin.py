a49e978fffed691de0e331ccb602aae23c5caf88
Daniel Greenfeld <pydanny@gmail.com>
2009-09-12 12:33:23 -0700

    Removing Rock's stuff for now so he can get it working. This way we can do a release today

--- tagging_ext/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from tagging_ext.models import SynonymTag, TaggedItem
-
-admin.site.register(SynonymTag)
-admin.site.register(TaggedItem)
-
--- tagging_ext/tests/test_basic_tagging.py
+++ tagging_ext/tests/test_basic_tagging.py
@@ -6,31 +6,13 @@ Tests basic django-tagging-ext functions
 from django.test import TestCase
 from django.test.client import Client
 
-from tagging.models import Tag
-from tagging_ext.models import SynonymTag, TaggedItem
-from tagging_ext.helpers import *
-
 class TestSynonyms(TestCase):
     
     def setUp(self):
-        self.tag_wisdom = Tag(name="Wisdom")
-        self.tag_wisdom.save()
-        self.tag_quote = Tag(name="Quote")
-        self.tag_quote.save()
-        self.stag_quotation = SynonymTag(name="Quotation",synonym=self.tag_quote)
-        self.stag_quotation.save()
-        SynonymTag.objects.create(name="Knowledge",synonym=self.tag_wisdom)
-        self.stag_quotes = SynonymTag(name="Quotes",synonym=self.stag_quotation)
-        self.stag_quotes.save()
+        pass
  
     def tearDown(self):
         pass
 
     def test_synonym_creation(self):
-        self.assertEquals( 5, Tag.objects.count())
-
-    def test_synonym_helper(self):
-        # import pdb; pdb.set_trace()
-        self.assertEquals( self.tag_quote, synonym( "Quote" ))
-        self.assertEquals( self.tag_quote, synonym( "Quotation" ))
-        self.assertEquals( self.tag_quote, synonym( "Quotes" ))
+        pass
