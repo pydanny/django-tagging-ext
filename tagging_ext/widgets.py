9581a86e1658ecc8aabb01ce9cf6b53f1000a7c9
Skylar Saveland <skylar.saveland@gmail.com>
2010-05-14 05:12:19 +0800

    make STATICURL optional in the auotcomplete widget

--- tagging_ext/widgets.py
+++ tagging_ext/widgets.py
@@ -8,14 +8,19 @@ from django.utils.safestring import mark_safe
 class TagAutoCompleteInput(forms.TextInput):
     
     class Media:
+        if hasattr(settings, 'STATIC_URL'):
+            media_url = settings.STATIC_URL
+        else:
+            media_url = settings.MEDIA_URL
+
         css = {
-            "all": (settings.STATIC_URL + "pinax/css/jquery.autocomplete.css",)
+            "all": (media_url + "pinax/css/jquery.autocomplete.css",)
         }
         js = (
-            settings.STATIC_URL + "pinax/js/jquery-1.3.2.min.js",
-            settings.STATIC_URL + "pinax/js/jquery.bgiframe.min.js",
-            settings.STATIC_URL + "pinax/js/jquery.ajaxQueue.js",
-            settings.STATIC_URL + "pinax/js/jquery.autocomplete.min.js"
+            media_url + "pinax/js/jquery-1.3.2.min.js",
+            media_url + "pinax/js/jquery.bgiframe.min.js",
+            media_url + "pinax/js/jquery.ajaxQueue.js",
+            media_url + "pinax/js/jquery.autocomplete.min.js"
         )
     
     def init(self, *args, **kwargs):
